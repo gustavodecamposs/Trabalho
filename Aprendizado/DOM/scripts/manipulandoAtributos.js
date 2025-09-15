40926a7861b0e8c4740cec530f9cd6aca30566f4
Gustavo <gustavoo.dcsilva@gmail.com>
2025-09-15 15:32:56 -0300

    Manipulando CSS e atributos

--- Aprendizado/DOM/scripts/manipulandoAtributos.js
+++ Aprendizado/DOM/scripts/manipulandoAtributos.js
@@ -1,5 +1,12 @@
 function clicou() {
     const input = document.querySelector('input');
+    const botao = document.querySelector('.botao');
 
-    input.setAttribute('placeholder', 'Placeholder alterado');
+    if (input.getAttribute('type') === 'text') {
+        input.setAttribute('type', 'password');
+        botao.innerText = 'Mostrar senha';
+    } else {
+        input.setAttribute('type', 'text');
+        botao.innerText = 'Ocultar senha';
+    }
 }
